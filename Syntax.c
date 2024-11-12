15048f4f6a6de14a9dfb88556feddc88cceab99b
Fedor212 <62344508+Fedor212@users.noreply.github.com>
2024-11-12 12:22:57 +0300

    Add files via upload

---
+++ Syntax.c
@@ -0,0 +1,25 @@
+
+// Functions
+int abss (int a)
+{
+	if (a>=0)	
+	return a;
+	return -a;
+}
+
+//  Cycles
+for (int i=11; i<=a; i++)
+{
+	
+}
+
+//  Cycles
+for (; i<=a;)  //Без некоторых аргументов
+{
+	
+}
+
+ // Ternarnik
+ min = a%10 < min ? a%10 : min;
+ 
+ sum==0 ? printf ("YES") : printf ("NO");
