b0625220acea5ca7b2e5c0c5630aad5c02cfad06
Fedor212 <62344508+Fedor212@users.noreply.github.com>
2024-11-17 04:06:11 +0300

    Update C12.c

--- HW6_C/C12.c
+++ HW6_C/C12.c
@@ -1,40 +1,79 @@
-/*НОД
-Составить функцию, которая определяет наибольший общий делитель двух натуральных и привести пример ее использования.
-int nod(int a, int b)
+/*Вычислить sin
+Составить функцию, которая вычисляет синус как сумму ряда (с точностью 0.001)
+sin(x) = x - x3/3! + x5/5! - x7/7! + ...(x в радианах)
+float sinus(float x)
 Формат входных данных
-Два целых положительных числа
+Одно число от 0 до 90
 Формат результата
-Одно целое число - наибольший общий делитель.
+Вещественное число в формате "%.3f"
 Примеры
 Входные данные
-14 21
+45
 Результат работы
-7 */
+0.707
+Входные данные
+30
+Результат работы
+0.500
+Входные данные
+0
+Результат работы
+0.000 */
 
 #include <math.h>
 #include <stdio.h>
 #include <stdint.h>
 //#include <locale.h>
 
-
-
-float sinn(float x)
+int a;
+int x;
+const double pi = 3.14159265358979323846;
+float sinn(int x)
 {
-   float b, c, d;
 
-        for (int i=3; i<=a; i=i+2)
-        {
-            c*=i;
+    float summ=0;
+    //float si;
 
-        }
-    return x;
+    for (int j=1; j<=9; j=j+2)
+    {
+
+
+        float f=1;
+        float p;
+        //float c;
+        for (int i = 1; i <=j; i++)  // Факториал
+            {
+                f*=i;
+            }
+        printf("%.3f    ", f);
+
+
+
+
+        p=pow(x, j);          //Степень
+        if(j%4==3)
+            p=-p;
+        printf("%.3f\n ", p);
+
+
+        summ = summ+p/f;
+        printf("%.3f\n", summ);
+        //summ = summ + f;
+        // printf ("%.2f ", summ);
+
+
+    }
+    //~ si=x-summ;
+
+    return summ;
 }
 
-int a, b;
+
 
 int main(void)
 {
-    scanf("%d%d", &a, &b);
-    printf ("%d", nod(a,b));
+    scanf("%d", &a);
+    sinn(a);
+    //printf ("%.3f", sinn(a));
     return 0;
 }
