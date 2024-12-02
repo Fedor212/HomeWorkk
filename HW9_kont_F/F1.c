f57898f812f503a170a82533492b084b3cfda5de
Fedor212 <62344508+Fedor212@users.noreply.github.com>
2024-12-02 10:58:53 +0300

    Update F1.c

--- HW9_kont_F/F1.c
+++ HW9_kont_F/F1.c
@@ -1,18 +1,9 @@
-/*Сумма цифр четна
-Составить функцию логическую функцию, которая определяет, верно ли, что сумма его цифр – четное число. Используя эту функцию решить задачу.
-Формат входных данных
-Одно целое не отрицательное число
-Формат результата
-Ответ YES или NO
-Примеры
-Входные данные
-136
-Результат работы
-YES
-Входные данные
-245
-Результат работы
-NO */
+/*Написать только одну функцию, которая сортирует массив по возрастанию.
+ * Необходимо реализовать только одну функцию, всю программу составлять не надо.
+Строго согласно прототипу. Имя функции и все аргументы должны быть:
+ void sort_array(int size, int a[]).
+Всю программу загружать не надо, только одну эту функцию.
+ Можно просто закомментировать текст всей программы, кроме данной функции */
 
 #include <math.h>
 #include <stdio.h>
@@ -21,17 +12,16 @@ NO */
 //#include <locale.h>
 #define SIZE 5
 
-int b[SIZE]={3, 5, 12, 8, 45};
-// Print Massive
-void printmass (int *arr, int len)
-{
-    for (int i = 0; i < len; i++)
-    {
-        printf ("%d ", arr[i]);
-    }
-}
-//void bricksort (int *arr, int len)
-// grow_up
+//~ int b[SIZE]={3, 5, 12, 8, 1};
+//~ // Print Massive
+//~ void printmass (int *arr, int len)
+//~ {
+    //~ for (int i = 0; i < len; i++)
+    //~ {
+        //~ printf ("%d ", arr[i]);
+    //~ }
+//~ }
+
 void sort_array(int size, int a[])
 {
     for (int i = 0; i < size; i++)
@@ -51,12 +41,13 @@ void sort_array(int size, int a[])
 
 
 
-int main(void)
-{
-    printmass (b, SIZE);
-    sort_array(SIZE, b);
-    printmass (b, SIZE);
+//~ int main(void)
+//~ {
+    //~ printmass (b, SIZE);
+    //~ printf ("\n");
+    //~ sort_array(SIZE, b);
+    //~ printmass (b, SIZE);
 
 
-    return 0;
-}
+    //~ return 0;
+//~ }
