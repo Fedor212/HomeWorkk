3817f5856567b73974c1649667d75c9033914995
Fedor212 <62344508+Fedor212@users.noreply.github.com>
2024-12-16 19:01:28 +0300

    Add files via upload

---
+++ Kursovaja/temp_api.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "temp_api.h"
+
+
+
+
+void mounth_avarage_temp(void)
+    {
+           
+    }
+
+void mounth_min_temp(void)
+    {
+           
+    }
+
+void mounth_max_temp(void)
+    {
+           
+    }
+
+void year_avarage_temp(void)
+    {
+           
+    }
+
+void year_min_temp(void)
+    {
+           
+    }
+
+void year_max_temp(void)
+    {
+           
+    }
+
+void hello_world(void)
+    {
+        printf("Hello!!! VS Code!");       
+    }
---
+++ Kursovaja/temp_api.h
@@ -0,0 +1,9 @@
+void mounth_avarage_temp(void);
+void mounth_min_temp(void);
+void mounth_max_temp(void);
+void year_avarage_temp(void);
+void year_min_temp(void);
+void year_max_temp(void); 
+void hello_world(void);
+
+extern int m; //Глоб переменная видна из обоих файлов
